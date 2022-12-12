70740ee57dbd88d1af3950a507992b595923d43c
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2022-12-12 17:25:29 +0100

    fix: fixed printf inside entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ ARGS=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$ARGS"
 
 exec env ${ARGS}
